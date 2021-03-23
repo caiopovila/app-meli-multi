4c12c1d2c4cc56afe9d5d52f04998e8b9e39cb2f
caiopovila <caiopovila@gmail.com>
2021-03-23 14:32:05 -0300

    delete pipe key verification

--- src/app/pipes/question-key-verify.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { QuestionKeyVerifyPipe } from './question-key-verify.pipe';
-
-describe('QuestionKeyVerifyPipe', () => {
-  it('create an instance', () => {
-    const pipe = new QuestionKeyVerifyPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
