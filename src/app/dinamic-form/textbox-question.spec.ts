1370f6e6bb2f1ac2b3842638d17924890be2969e
caiopovila <caiopovila@gmail.com>
2021-03-23 14:24:28 -0300

    remove dinamic

--- src/app/dinamic-form/textbox-question.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TextboxQuestion } from './textbox-question';
-
-describe('TextboxQuestion', () => {
-  it('should create an instance', () => {
-    expect(new TextboxQuestion()).toBeTruthy();
-  });
-});
